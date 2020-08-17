1f8442328519b8b23684f37c794a5ee0e84c6cea
Meguriau <raze@live.cn>
2020-08-17 23:46:36 +0800

    Update.

--- update.sh
+++ update.sh
@@ -7,7 +7,6 @@ git push -v origin refs/heads/master\:refs/heads/master
 
 git checkout gitlab
 git merge --no-edit master
-git push -v upstream refs/heads/gitlab\:refs/heads/gitlab
 git push -v upstream gitlab\:refs/heads/master
 
 git checkout master
