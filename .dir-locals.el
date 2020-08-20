aab3ef4a2579d1feaee0ab487ea5813d8773e253
Meguriau <raze@live.cn>
2020-08-20 09:12:31 +0800

    Update .dir-locals.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,10 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((eval . (setq-local org-download-image-dir
+((org-mode . ((org-download--dir-2-overridden . "index")
+              (org-display-custom-times . t)
+              (org-time-stamp-custom-formats . ("<%Y年%m月%d日>" . "<%Y年%m月%d日 %H时%M分>"))
+              (eval . (setq-local org-download-image-dir
                                   (expand-file-name "images"
                                                     (file-name-directory
                                                      (buffer-file-name))))))))
